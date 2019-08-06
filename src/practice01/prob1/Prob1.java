ec47de77586aca0ffaced0089845292989cff9e8
bit <bit@DESKTOP-S8HQR48>
2019-08-06 16:16:48 +0900

    3번 완료

--- src/practice01/prob1/Prob1.java
+++ src/practice01/prob1/Prob1.java
@@ -6,7 +6,7 @@ public class Prob1 {
 	public static void main (String args[]) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("수를 입력하세요 : ");
+		System.out.print("수를 입력하세요 : ");
 		int number = sc.nextInt();
 		
 		if(number%3==0) System.out.println("3의 배수입니다.");
--- src/practice01/prob3/Prob3.java
+++ src/practice01/prob3/Prob3.java
@@ -1,6 +1,25 @@
 package practice01.prob3;
 
+import java.util.Scanner;
+
 public class Prob3 {
 	public static void main (String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자를 입력하세요 : ");
+		
+		int num = sc.nextInt();
+		int sum = 0;
+		int k;
+		
+		if(num%2==0) k=2;
+		else k=1;
+		
+		for (int i = k; i <= num; i+=2) {
+			sum+=i;
+		}
+		
+		System.out.println(sum);
+		sc.close();
 	}
 }
