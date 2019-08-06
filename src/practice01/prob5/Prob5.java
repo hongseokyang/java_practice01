16f20f993aa83418b628ea609ac29a13b6254bb0
bit <bit@DESKTOP-S8HQR48>
2019-08-06 19:12:16 +0900

    5번 완료

--- src/practice01/prob5/Prob5.java
+++ src/practice01/prob5/Prob5.java
@@ -2,5 +2,19 @@ package practice01.prob5;
 
 public class Prob5 {
 	public static void main (String args[]) {
+		for (int i = 1; i <= 99; i++) {
+			String s = Integer.toString(i);
+			int count = 0;
+			for (int j = 0; j < s.length(); j++) {
+				if((s.charAt(j)-'0')%3==0 && (s.charAt(j)-'0')>=3) count++;
+			}
+			if(count>0) {
+				System.out.print(s+" ");
+				for (int j = 0; j < count; j++) {
+					System.out.print("짝");
+				}
+				System.out.println();
+			}
+		}
 	}
 }
